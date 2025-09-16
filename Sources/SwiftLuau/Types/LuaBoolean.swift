db51915c7793defdb4a0df578bcef645db66ecd5
Josh <josh@bashed.sh>
2025-09-16 11:53:23 -0700

    refactor: remove unused methods and fix stack bugs

--- Sources/SwiftLuau/Types/LuaBoolean.swift
+++ Sources/SwiftLuau/Types/LuaBoolean.swift
@@ -61,33 +61,4 @@ public struct LuaBoolean: Sendable, LuaPushable, LuaGettableNonOptional {
         Lua.pop(state, 1)
         return boolValue
     }
-
-    // /// Push a Swift boolean onto the Lua stack.
-    // /// - Parameters:
-    // ///   - value: The boolean value to push.
-    // ///   - state: The Lua state to push to.
-    // public static func push(_ value: Bool, to state: LuaState) {
-    //     lua_pushboolean(state.state, value ? 1 : 0)
-    // }
-
-    // /// Get a Swift boolean from the Lua stack.
-    // /// - Parameters:
-    // ///   - index: The stack index to get the value from.
-    // ///   - state: The Lua state to get the value from.
-    // /// - Returns: The boolean value if it exists and is a boolean, nil otherwise.
-    // public static func get(from state: LuaState, at index: Int32) -> Bool? {
-    //     guard LuaType.get(from: state, at: index) == .boolean else {
-    //         return nil
-    //     }
-    //     return lua_toboolean(state.state, index) != 0
-    // }
-
-    // /// Get a Swift boolean from the Lua stack, converting the value to a boolean if necessary.
-    // /// - Parameters:
-    // ///   - index: The stack index to get the value from.
-    // ///   - state: The Lua state to get the value from.
-    // /// - Returns: The boolean value if it exists or can be converted to a boolean, nil otherwise.
-    // public static func getConverting(from state: LuaState, at index: Int32) -> Bool {
-    //     return lua_toboolean(state.state, index) != 0
-    // }
 }
--- Sources/SwiftLuau/Types/LuaUserdata.swift
+++ Sources/SwiftLuau/Types/LuaUserdata.swift
@@ -62,35 +62,7 @@ public struct LuaUserdata: Sendable, LuaPushable, LuaGettable {
             return nil
         }
         let userdata = lua_touserdata(state.state, -1)
+        Lua.pop(state, 1)
         return userdata
     }
-
-    // /// Push a userdata onto the Lua stack.
-    // /// - Parameters:
-    // ///   - userdata: The userdata to push.
-    // ///   - state: The Lua state to push to.
-    // public static func push(_ userdata: LuaUserdata, to state: LuaState) {
-    //     userdata.buffer.withUnsafeBytes { rawBuffer in
-    //         guard let baseAddress = rawBuffer.baseAddress else {
-    //             fatalError("Failed to get base address of userdata buffer")
-    //         }
-    //         let ptr = lua_newuserdatadtor(state.state, rawBuffer.count, userdata.deallocater)
-    //         if let dst = ptr {
-    //             dst.copyMemory(from: baseAddress, byteCount: rawBuffer.count)
-    //         }
-    //     }
-    // }
-
-    // /// Get a userdata from the Lua stack.
-    // /// - Parameters:
-    // ///   - index: The stack index to get the value from.
-    // ///   - state: The Lua state to get the value from.
-    // /// - Returns: The userdata if it exists and is a userdata, nil otherwise.
-    // public static func get(from state: LuaState, at index: Int32) -> UnsafeMutableRawPointer? {
-    //     if LuaType.get(from: state, at: index) != .userdata {
-    //         return nil
-    //     }
-    //     let userdata = lua_touserdata(state.state, index)
-    //     return userdata
-    // }
 }
