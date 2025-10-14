e1f40bbe03e1396d85f121c628456539e03acf8c
Josh <josh@bashed.sh>
2025-10-14 02:07:26 -0700

    chore: update to luau 0.695

---
+++ scripts/updateLuau.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Local dir
+CWD=$(pwd)
+DIR=$(cd "$(dirname "$0")" || exit; pwd)
+ROOT_DIR=$(dirname "$DIR")
+
+cd "$ROOT_DIR/lib/Luau" || exit
+
+# Get latest tags
+git fetch --tags
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "Updating Luau to latest tag: $LATEST_TAG"
+git checkout "$LATEST_TAG"
+
+$DIR/fixSymlinks.sh
+
+cd "$CWD" || exit
+
+exit 0
